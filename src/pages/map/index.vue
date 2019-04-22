3eb91bad4b41b6f244bb2a9e6c3b9d8345cdb506
li <993997685@qq.com>
2019-04-22 20:09:15 +0800

    初步完成签到页面 还差个函数

--- src/pages/map/index.vue
+++ src/pages/map/index.vue
@@ -1,49 +1,119 @@
 <template>
   <div>
     <map id="map" 
-      @latitude = "latitude" 
-      @longitude = "longitude" 
-      scale="14" 
+      :longitude = "longitude" 
+      :latitude = "latitude" 
+      scale="18" 
+      :markers="markers" 
+      :circle="circle" 
       show-location 
-      style="width: 100%; height: 700rpx;">
+      style="width: 100%; height: 800rpx;">
     </map>
+    <div class="site">
+      <img src="../../../static/images/定位.png" alt="">
+      <p class="now">当前位置：</p>
+      <p class="nowsite">{{nowsite}}</p>
+    </div>
+    <button @click="sign">确认签到</button>
   </div>
 </template>
 
 <script>
+import QQMapWX from '../../../static/qqmap-wx-jssdk.js';
+
 export default {
   data () {
     return {
       // 指定经纬度
-      aim_latitude: 32.060255,
-      aim_longitude: 118.796877,
+      aim_latitude: 32.113950,
+      aim_longitude: 118.92920,
       // 当前经纬度
       latitude: '',
-      longitude: ''
+      longitude: '',
+      markers: [{
+        iconPath: '../../../static/images/定位.png',
+        id: 0,
+        latitude: 32.113950,
+        longitude: 118.92920,
+        width: 30,
+        height: 30
+      }],
+      circle: [{
+        latitude: 32.113950,
+        longitude: 118.92920,
+        color: '#666',
+        fillColor: '#aaa',
+        radius: 3000
+      }],
+      nowsite: ''   // 当前位置
     }
   },
-  // mathod: {
-  //   getLocation () {
-  //     wx.getLocation({
-  //       type: 'wgs84',
-  //       success (res) {
-  //         console.log('lication', res)
-  //       }
-  //     })
-  //   }
-  // }
+
+  methods: {
+    getAddress (lat,lng) {
+      let that = this;
+      let qqmapsdk = new QQMapWX({
+        key: 'XEOBZ-WMEKU-MEEVF-2A3WV-XYRNK-WLBS6'
+      });
+      qqmapsdk.reverseGeocoder({
+        location: {
+          latitude: lat,
+          longitude: lng
+        },
+        success (res) {
+          console.log('success', res.result.address);
+          that.nowsite =  res.result.address;
+        },
+        fail (res) {
+          console.log('error');
+        }
+      })
+    },
+    sign () {
+      let lat = wx.getStorageSync('latitude');
+      let lng = wx.getStorageSync('longitude');
+      if(Math.sqrt(Math.pow(lat - this.aim_latitude,2)+Math.pow(lng - this.aim_longitude,2)) > 30){
+        console.log('成功');
+      }
+    }
+  },
   onLoad: function (options) {
     let that = this
     wx.getLocation({
-      type: 'wgs84',
+      type: 'gcj02',
       success (res) {
-        console.log('lication', res)
-        console.log('llllllllllllaaaaaaaaaaa', res.latitude)
-        that.latitude = res.latitude
-        that.longitude = res.longitude
+        console.log('lication', res);
+        that.latitude = res.latitude;
+        that.longitude = res.longitude;
+        wx.setStorageSync('latitude', res.latitude);
+        wx.setStorageSync('longitude', res.longitude);
       }
-    })
+    });
+    let lat = wx.getStorageSync('latitude');
+    let lng = wx.getStorageSync('longitude');
+    that.getAddress(lat,lng);
   }
 }
 </script>
+<style>
+  .site{
+    margin: 20rpx;
+    border: solid 1px #bdbdbd;
+    border-radius: 20rpx;
+    position: relative;
+  }
+  .site img{
+    width: 80rpx;
+    height: 80rpx;
+    margin: 20rpx;
+  }
+  .site .now{
+    position: absolute;
+    left: 120rpx;
+    top: 40rpx;
+  }
+  .site .nowsite{
+    padding: 0 40rpx 40rpx 20rpx;
+  }
 
+</style>
